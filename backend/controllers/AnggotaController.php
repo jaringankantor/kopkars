50ac98c8ac39cfb05973d7675379bb57921558a5
JaringanKantor.com <nc.anwar@jaringankantor.com>
2021-12-14 09:38:16 +0700

    Update AnggotaController.php

--- backend/controllers/AnggotaController.php
+++ backend/controllers/AnggotaController.php
@@ -227,6 +227,8 @@ class AnggotaController extends Controller
     protected function findModelAnggota($id)
     {
         if (($model = Anggota::findOneAnggota($id)) !== null) {
+            print_r($model);
+            exit();
             return $model;
         }
 
