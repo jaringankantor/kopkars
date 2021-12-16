609081484f411d98b37048255f632546ee087a16
JaringanKantor.com <nc.anwar@jaringankantor.com>
2021-12-16 16:08:48 +0700

    Update import-tokopedia.php

--- backend/views/transaksi/import-tokopedia.php
+++ backend/views/transaksi/import-tokopedia.php
@@ -126,7 +126,7 @@ $this->title = 'Import Tokopedia';
                         $model->waktu = $waktu;
                         $model->insert_by = Yii::$app->user->identity->email;
 
-                        if (!$model->save()) $sum_error++;
+                        if (!$model->save()) $print_r($model);
                     }
                 }
             }
