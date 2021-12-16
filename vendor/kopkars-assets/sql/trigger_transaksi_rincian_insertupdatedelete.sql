04f5e871114cf88cbb718e88f0b4cc59638fb260
JaringanKantor.com <nc.anwar@jaringankantor.com>
2021-12-16 23:21:53 +0700

    Update trigger_transaksi_rincian_insertupdatedelete.sql

--- vendor/kopkars-assets/sql/trigger_transaksi_rincian_insertupdatedelete.sql
+++ vendor/kopkars-assets/sql/trigger_transaksi_rincian_insertupdatedelete.sql
@@ -36,14 +36,14 @@ BEGIN
             FROM transaksi_rincian
             WHERE kode_toko = NEW.kode_toko AND kanal_transaksi = NEW.kanal_transaksi AND nomor_referensi = NEW.nomor_referensi;
 
-            WITH updated AS (UPDATE transaksi
+            UPDATE transaksi
             SET subtotal = sum_subtotal, diskon = sum_diskon, pajak=sum_pajak, total_penjualan = sum_total_penjualan,
             pembayaran = sum_pembayaran, saldo = sum_saldo, keterangan = all_keterangan
             WHERE kode_toko = NEW.kode_toko AND kanal_transaksi = NEW.kanal_transaksi AND nomor_referensi = NEW.nomor_referensi
-            RETURNING id INTO last_transaksi_id)
+            RETURNING id INTO last_transaksi_id;
 
             UPDATE transaksi_rincian
-            SET transaksi_id = updated
+            SET transaksi_id = last_transaksi_id
             WHERE kode_toko = NEW.kode_toko AND kanal_transaksi = NEW.kanal_transaksi AND nomor_referensi = NEW.nomor_referensi;
 
         END IF;
