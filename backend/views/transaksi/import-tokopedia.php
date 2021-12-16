66a30d9534b050a00a4683d2c78906269c4eac6b
JaringanKantor.com <nc.anwar@jaringankantor.com>
2021-12-16 15:55:41 +0700

    Update import-tokopedia.php

--- backend/views/transaksi/import-tokopedia.php
+++ backend/views/transaksi/import-tokopedia.php
@@ -77,7 +77,7 @@ $this->title = 'Import Tokopedia';
 
                 $subtotal = $harga_awal*$jumlah_barang;
 
-                if($nomor_referensi='INV/20200927/XX/IX/638680882'){
+                if($nomor_referensi=='INV/20200927/XX/IX/638680882' AND $nama_produk=='Bakteri Baik EM4 1 Liter Perikanan dan Tambak'){
                 print_r($harga_awal.$jumlah_barang);
                 exit();
             }
