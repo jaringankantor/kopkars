e8570eb3c448416a4f005c5dd5e463fbd4c70eaf
JaringanKantor.com <nc.anwar@jaringankantor.com>
2021-12-17 11:14:07 +0700

    Update import-lazada.php

--- backend/views/transaksi/import-lazada.php
+++ backend/views/transaksi/import-lazada.php
@@ -137,7 +137,11 @@ $this->title = 'Import Lazada';
                         $model->waktu = $waktu;
                         $model->insert_by = Yii::$app->user->identity->email;
 
-                        if (!$model->save())$sum_error++;
+                        if (!$model->save()) {
+                            print_r($model);
+                            exit();
+                            $sum_error++;
+                        }
                     } else {
                         $model = TransaksiRincian::findOneTransaksiRincian($transaksi_rincian_id);
                         if(strpos($model->nomor_referensi_rincian,$nomor_referensi_rincian)){
