e9f0ca661aefe4e997d3883f5e46376f7d436594
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-04 15:24:52 +0700

    Update index.php

--- backend/views/anggota/index.php
+++ backend/views/anggota/index.php
@@ -19,79 +19,77 @@ $this->title = 'Anggota Aktif';
 
     <?php echo $this->render('_search_index', ['model' => $searchModel]); ?>
 
+    <br>
+
     <?php
 
     $gridColums = [
         ['class' => 'yii\grid\SerialColumn'],
 
-            'nomor_anggota',
-            'nama_lengkap',
-            [
-                'class' => 'kartik\grid\EditableColumn',
-                'attribute' => 'email',
-                'editableOptions' => [
-                    'formOptions' => [
-                        'action' => Url::to(['/anggota/updateemail-editable-json']),
-                    ],
+        'nomor_anggota',
+        'nama_lengkap',
+        [
+            'class' => 'kartik\grid\EditableColumn',
+            'attribute' => 'email',
+            'editableOptions' => [
+                'formOptions' => [
+                    'action' => Url::to(['/anggota/updateemail-editable-json']),
                 ],
             ],
-            [
-                'class' => 'kartik\grid\EditableColumn',
-                'attribute' => 'nomor_hp',
-                'value'=>function($model, $key, $index)
-                {return '+62'.$model->nomor_hp;},
-                'editableOptions' => [
-                    'formOptions' => [
-                        'action' => Url::to(['/anggota/updatehp-editable-json']),
-                    ],
+        ],
+        [
+            'class' => 'kartik\grid\EditableColumn',
+            'attribute' => 'nomor_hp',
+            'value'=>function($model, $key, $index){
+                return '+62'.$model->nomor_hp;
+            },
+            'editableOptions' => [
+                'formOptions' => [
+                    'action' => Url::to(['/anggota/updatehp-editable-json']),
                 ],
             ],
-            //[
-            //    'attribute'=>'Informasi Kontak',
-            //    'format'=>'raw',
-            //    'value'=>function($model, $key, $index)
-            //    {return 'Email: '.$model->email.'<br>No HP: +62'.$model->nomor_hp;},
-            //],
+        ],
+        //[
+        //    'attribute'=>'Informasi Kontak',
+        //    'format'=>'raw',
+        //    'value'=>function($model, $key, $index)
+        //    {return 'Email: '.$model->email.'<br>No HP: +62'.$model->nomor_hp;},
+        //],
 
-            ['class' => 'yii\grid\ActionColumn',
-                'template' => '{view} {update} {delete}',
-                'buttons' => [
-                    'view' => function ($url, $model) {
-                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
-                                    'title' => Yii::t('app', 'lead-view'),
-                        ]);
-                    },
-        
-                    'update' => function ($url, $model) {
-                        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
-                                    'title' => Yii::t('app', 'lead-update'),
-                        ]);
-                    },
-                    'delete' => function ($url, $model) {
-                        return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
-                                    'title' => Yii::t('app', 'lead-delete'),
-                        ]);
-                    }
-        
-                ],
-                'urlCreator' => function ($action, $model, $key, $index) {
-                    if ($action === 'view') {
-                        return ['anggota/view-biodata','id'=>$model->id];
-                    }
-                    if ($action === 'update') {
-                        return ['anggota/update','id'=>$model->id];
-                    }
-                    if ($action === 'delete') {
-                        return ['anggota/delete','id'=>$model->id];
-                    }
-        
+        ['class' => 'yii\grid\ActionColumn',
+        'template' => '{view} {update} {delete}',
+        'buttons' =>
+            [
+                'view' => function ($url, $model) {
+                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, ['title' => Yii::t('app', 'lead-view'),]);
                 },
-                'visibleButtons' => [
-                    'delete' => function ($model, $key, $index) {
-                        return empty($model->nomor_anggota) ? true : false;
-                    }
-                ]
+                'update' => function ($url, $model) {
+                    return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url,
+                    ['title' => Yii::t('app', 'lead-update'),]);
+                },
+                'delete' => function ($url, $model) {
+                    return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url,
+                    ['title' => Yii::t('app', 'lead-delete'),]);
+                }
             ],
+        'urlCreator' => function ($action, $model, $key, $index) {
+            if ($action === 'view') {
+                return ['anggota/view-biodata','id'=>$model->id];
+            }
+            if ($action === 'update') {
+                return ['anggota/update','id'=>$model->id];
+            }
+            if ($action === 'delete') {
+                return ['anggota/delete','id'=>$model->id];
+            }
+        },
+        'visibleButtons' =>
+            [
+                'delete' => function ($model, $key, $index) {
+                    return empty($model->nomor_anggota) ? true : false;
+                }
+            ]
+        ],
     ];
     
     echo GridView::widget([
