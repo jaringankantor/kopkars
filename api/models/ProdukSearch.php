7e8fad0b5735b623cff470b735d04e0248a53797
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-09 23:47:32 +0700

    Update ProdukSearch.php

--- api/models/ProdukSearch.php
+++ api/models/ProdukSearch.php
@@ -6,5 +6,14 @@ use common\models\ProdukSearch as CommonProdukSearch;
 
 class ProdukSearch extends CommonProdukSearch
 {
-
+    public $sku;
+    public $nama_produk;
+    
+    public function rules()
+    {
+        return [
+            ['sku', 'string', 'min' => 6],
+            ['nama_produk', 'string', 'min' => 4],              
+        ];
+    }
 }
