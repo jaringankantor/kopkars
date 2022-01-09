c160e74a6df7f75be8ad2db8ff6ac6ceaf040486
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-09 19:42:16 +0700

    Update Produk.php

--- api/models/Produk.php
+++ api/models/Produk.php
@@ -31,25 +31,25 @@ class Produk extends CommonProduk {
         ];
     }
 
-    public function extraFields()
-    {
-        return ['deskripsi',
-        'url_image_1' => function($model){
-            return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,1);
-        },
-        'url_image_2' => function($model){
-            return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,2);
-        },
-        'url_image_3' => function($model){
-            return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,3);
-        },
-        'url_image_4' => function($model){
-            return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,4);
-        },
-        'url_image_5' => function($model){
-            return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,5);
-        }];
-    }
+    // public function extraFields()
+    // {
+    //     return ['deskripsi',
+    //     'url_image_1' => function($model){
+    //         return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,1);
+    //     },
+    //     'url_image_2' => function($model){
+    //         return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,2);
+    //     },
+    //     'url_image_3' => function($model){
+    //         return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,3);
+    //     },
+    //     'url_image_4' => function($model){
+    //         return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,4);
+    //     },
+    //     'url_image_5' => function($model){
+    //         return Yii::$app->kopkarstext->urlFotoProdukBackend($model->kode_toko,$model->sku,5);
+    //     }];
+    // }
 
     public static function findProdukAktifByKodeToko($kode_toko)
     {
