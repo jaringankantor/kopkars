6ee84cc578a23b2217623d647ad2142e1b91f81b
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-09 23:33:34 +0700

    Update ProdukSearch.php

--- common/models/ProdukSearch.php
+++ common/models/ProdukSearch.php
@@ -19,7 +19,7 @@ class ProdukSearch extends Produk
     public function rules()
     {
         return [
-            [['keyword','nama_produk'], 'safe'],
+            [['keyword','nama_produk','sku'], 'safe'],
         ];
     }
 
