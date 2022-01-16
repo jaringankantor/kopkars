d96c24cee81c01b827738b0fae7349afdab3c6f9
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-16 15:19:33 +0700

    Update index.php

--- frontend/views/cicilan/index.php
+++ frontend/views/cicilan/index.php
@@ -14,12 +14,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Cicilan', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
