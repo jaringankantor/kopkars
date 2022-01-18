5d2a9b0e974c71846efc4e2477c8e60fb3f653dd
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-18 16:23:30 +0700

    Update index.php

--- backend/views/voucher/index.php
+++ backend/views/voucher/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use common\models\HistoriAnggotaSimpanan;
+use kartik\export\ExportMenu;
 use yii\helpers\Html;
 use yii\helpers\Url;
 //use yii\grid\GridView;
@@ -18,46 +20,91 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+    <br>
 
-            ['label'=>'Nama Anggota Kopkars', 'value'=>function ($model, $index, $widget) { return $model->anggota->nama_lengkap; }],
-            'kode_voucher',
-            //'kode_toko',
-            'rupiah:currency',
-            'rupiah_terpakai:currency',
-            //'berlaku_mulai:date',
-            'berakhir_sampai:date',
-            [
-                'class' => 'kartik\grid\EditableColumn',
-                'attribute' => 'keterangan',
-                'value'=>function($model, $key, $index){
-                    return $model->keterangan;
-                },
-                'editableOptions' => [
-                    'formOptions' => [
-                        'action' => Url::to(['/voucher/update-editable-json']),
-                    ],
-                ],
-            ],
+    <?php
 
-            ['class' => 'yii\grid\ActionColumn',
-                'template' => '{gunakan}',
-                'buttons' => [
-                    'gunakan' => function ($url,$model) {
-                        if (($model->rupiah_terpakai < $model->rupiah) AND (strtotime($model->berakhir_sampai) > strtotime(date("Y-m-d H:is")))){ 
-                            return Html::a('<span class="glyphicon glyphicon-ok"></span> Gunakan', ['ok-gunakan','kode_voucher'=>$model->kode_voucher,'kode_toko'=>$model->kode_toko], ['id'=>'approved-button'.$model->kode_toko.$model->kode_voucher,'class' => 'btn btn-success','data' => ['confirm' => 'Gunakan voucher ini??','method' => 'post']]);
-                        } else {
-                            return 'Sudah Digunakan';
-                        }
-                    }
+    $gridColumns = [
+        ['class' => 'yii\grid\SerialColumn'],
+
+        ['label'=>'Nama Anggota Kopkars', 'value'=>function ($model, $index, $widget) { return $model->anggota->nama_lengkap; }],
+        'kode_voucher',
+        //'kode_toko',
+        'rupiah:currency',
+        'rupiah_terpakai:currency',
+        //'berlaku_mulai:date',
+        'berakhir_sampai:date',
+        [
+            'class' => 'kartik\grid\EditableColumn',
+            'attribute' => 'keterangan',
+            'value'=>function($model, $key, $index){
+                return $model->keterangan;
+            },
+            'editableOptions' => [
+                'formOptions' => [
+                    'action' => Url::to(['/voucher/update-editable-json']),
                 ],
             ],
         ],
-    ]); ?>
+        'last_waktu_update',
+
+        ['class' => 'yii\grid\ActionColumn',
+            'template' => '{gunakan}',
+            'buttons' => [
+                'gunakan' => function ($url,$model) {
+                    if (($model->rupiah_terpakai < $model->rupiah) AND (strtotime($model->berakhir_sampai) > strtotime(date("Y-m-d H:is")))){ 
+                        return Html::a('<span class="glyphicon glyphicon-ok"></span> Gunakan', ['ok-gunakan','kode_voucher'=>$model->kode_voucher,'kode_toko'=>$model->kode_toko], ['id'=>'approved-button'.$model->kode_toko.$model->kode_voucher,'class' => 'btn btn-success','data' => ['confirm' => 'Gunakan voucher ini??','method' => 'post']]);
+                    } else {
+                        return 'Sudah Digunakan';
+                    }
+                }
+            ],
+        ],
+    ];
+
+    $customDropdown = [
+        //'linkOptions' => ['class' => 'dropdown-item']
+    ];
+
+    $fullExportMenu = ExportMenu::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $gridColumns,
+        'target' => ExportMenu::TARGET_BLANK,
+        'asDropdown' => false, // this is important for this case so we just need to get a HTML list 
+        'exportConfig' => [ // set styling for your custom dropdown list items
+            ExportMenu::FORMAT_CSV => $customDropdown,
+            ExportMenu::FORMAT_TEXT => $customDropdown,
+            ExportMenu::FORMAT_HTML => $customDropdown,
+            ExportMenu::FORMAT_PDF => $customDropdown,
+            ExportMenu::FORMAT_EXCEL => $customDropdown,
+            ExportMenu::FORMAT_EXCEL_X => $customDropdown,
+        ],
+    ]);
+    
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $gridColumns,
+        'panel' => [
+            'type' => GridView::TYPE_PRIMARY,
+            'heading' => '<h3 class="panel-title"><i class="fas fa-book"></i> '.$this->title.'</h3>',
+        ],
+        'exportContainer' => [
+            'class' => 'btn-group mr-2 me-2'
+        ],
+        'toolbar' => [
+            '{export}',
+        ],
+        'export' => [
+            'itemsAfter'=> [
+                '<div role="presentation" class="dropdown-divider"></div>',
+                '<div class="dropdown-header">Export All Data</div>',
+                $fullExportMenu
+            ]
+        ],
+        'options'=>['class'=>'box-body table-responsive no-padding'],
+    ]);
+    
+    ?>
 
 
 </div>
