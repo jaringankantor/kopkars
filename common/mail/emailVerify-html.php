c38a98ba2303865c8aefed61e723e11f7d52022e
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-19 15:51:31 +0700

    Update emailVerify-html.php

--- common/mail/emailVerify-html.php
+++ common/mail/emailVerify-html.php
@@ -4,10 +4,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $user common\models\User */
 
-$verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $user->verification_token]);
+$verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $anggota->verification_token]);
 ?>
 <div class="verify-email">
-    <p>Hallo <?= Html::encode($user->nama_lengkap) ?>,</p>
+    <p>Hallo <?= Html::encode($anggota->nama_lengkap) ?>,</p>
 
     <p>Klik link di bawah ini, setelah itu mohon memperbaharui biodata:</p>
 
