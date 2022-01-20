b0ec53e5a96247b81dfb692db5147299f075ae73
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-20 14:45:19 +0700

    Update index.php

--- backend/views/cicilan/index.php
+++ backend/views/cicilan/index.php
@@ -23,32 +23,74 @@ $this->title = 'Cicilan Anggota';
 
     <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+    <br>
 
-            ['label'=>'Nama Anggota Kopkars', 'value'=>function ($model, $index, $widget) { return $model->anggota->nama_lengkap; }],
-            'kanal_cicilan',
-            'nomor_referensi',
-            'cicilan:currency',
-            [
-                'class' => 'kartik\grid\EditableColumn',
-                'attribute' => 'keterangan',
-                'value'=>function($model, $key, $index){
-                    return $model->keterangan;
-                },
-                'editableOptions' => [
-                    'formOptions' => [
-                        'action' => Url::to(['/cicilan/update-keterangan-editable-json']),
-                    ],
+    <?php
+
+    $gridColumns = [
+        ['class' => 'yii\grid\SerialColumn'],
+
+        ['label'=>'Nama Anggota Kopkars', 'value'=>function ($model, $index, $widget) { return $model->anggota->nama_lengkap; }],
+        'kanal_cicilan',
+        'nomor_referensi',
+        'cicilan:currency',
+        [
+            'class' => 'kartik\grid\EditableColumn',
+            'attribute' => 'keterangan',
+            'value'=>function($model, $key, $index){
+                return $model->keterangan;
+            },
+            'editableOptions' => [
+                'formOptions' => [
+                    'action' => Url::to(['/cicilan/update-keterangan-editable-json']),
                 ],
             ],
-            'waktu:date',
-
-            // ['class' => 'yii\grid\ActionColumn'],
         ],
+        'waktu:date',
+
+        // ['class' => 'yii\grid\ActionColumn'],
+    ];
+
+    $customDropdown = [
+        //'linkOptions' => ['class' => 'dropdown-item']
+    ];
+
+    $fullExportMenu = ExportMenu::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $gridColumns,
+        'target' => ExportMenu::TARGET_BLANK,
+        'asDropdown' => false, // this is important for this case so we just need to get a HTML list 
+        'exportConfig' => [ // set styling for your custom dropdown list items
+            ExportMenu::FORMAT_CSV => $customDropdown,
+            ExportMenu::FORMAT_TEXT => $customDropdown,
+            ExportMenu::FORMAT_HTML => $customDropdown,
+            ExportMenu::FORMAT_PDF => $customDropdown,
+            ExportMenu::FORMAT_EXCEL => $customDropdown,
+            ExportMenu::FORMAT_EXCEL_X => $customDropdown,
+        ],
+    ]);
+    
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $gridColumns,
+        'panel' => [
+            'type' => GridView::TYPE_PRIMARY,
+            'heading' => '<h3 class="panel-title"><i class="fas fa-book"></i> '.$this->title.'</h3>',
+        ],
+        'exportContainer' => [
+            'class' => 'btn-group mr-2 me-2'
+        ],
+        'toolbar' => [
+            '{export}',
+        ],
+        'export' => [
+            'itemsAfter'=> [
+                '<div role="presentation" class="dropdown-divider"></div>',
+                '<div class="dropdown-header">Export All Data</div>',
+                $fullExportMenu
+            ]
+        ],
+        'options'=>['class'=>'box-body table-responsive no-padding'],
     ]); ?>
 
 
