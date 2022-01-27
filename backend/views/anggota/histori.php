b03a877dd61bfa27a334ca12dbbdce7899f66734
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-01-28 01:30:19 +0700

    Update histori.php

--- backend/views/anggota/histori.php
+++ backend/views/anggota/histori.php
@@ -20,6 +20,8 @@ $this->title = 'Histori Anggota';
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <p><a href="https://datastudio.google.com/u/0/reporting/abd7a6c3-5062-4dc5-baf8-de8659532e00/page/p_lyvat87mrc" target="_blank">Rekapitulasi Perubahan Anggota</a></p>
+
     <?php echo $this->render('_search_histori', ['model' => $searchModel]); ?>
 
     <br>
