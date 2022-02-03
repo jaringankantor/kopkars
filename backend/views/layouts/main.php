930b160c1e5014205b80b589e4d28bfda2349706
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-02-03 22:09:10 +0700

    Update main.php

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -71,7 +71,7 @@ AppAsset::register($this);
                         ['label' => 'Form', 'url' => ['/form']],
                         ['label' => 'Form Field', 'url' => ['/form-field']],
                     ],
-                    'submenuTemplate' => "\n<ul class='dropdown-menu' role='menu'>\n{items}\n</ul>\n",
+                    'submenuTemplate' => "\n<ul class='dropdown-menux' role='menu'>\n{items}\n</ul>\n",
                 ]
             ],
             'submenuTemplate' => "\n<ul class='dropdown-menu' role='menu'>\n{items}\n</ul>\n",
