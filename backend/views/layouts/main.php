67e4ae0803a2023e1c3cf3e9bdd77cd1723823f1
JaringanKantor.com <nc.anwar@jaringankantor.com>
2022-02-03 23:29:56 +0700

    Update main.php

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -41,16 +41,16 @@ AppAsset::register($this);
     $menuItems = [];
 
     if (Yii::$app->user->can('All Pengaturan Sistem') OR Yii::$app->user->can('All Sistem Marketplace')) {
-        if(Yii::$app->user->can('All Pengaturan Sistem')) {
-            $sistem[] = [
-                ['label' => 'Market Place', 'url' => ['/variabel-marketplace']],
-                ['label' => 'Toko', 'url' => ['/toko']],
-                ['label' => 'User', 'url' => ['/user']],
-                ['label' => 'Field', 'url' => ['/field']],
-                ['label' => 'Form', 'url' => ['/form']],
-                ['label' => 'Form Field', 'url' => ['/form-field']],
-            ];
-        }
+        // if(Yii::$app->user->can('All Pengaturan Sistem')) {
+        //     $sistem[] = [
+        //         ['label' => 'Market Place', 'url' => ['/variabel-marketplace']],
+        //         ['label' => 'Toko', 'url' => ['/toko']],
+        //         ['label' => 'User', 'url' => ['/user']],
+        //         ['label' => 'Field', 'url' => ['/field']],
+        //         ['label' => 'Form', 'url' => ['/form']],
+        //         ['label' => 'Form Field', 'url' => ['/form-field']],
+        //     ];
+        // }
         
         if(Yii::$app->user->can('All Sistem Marketplace')) {
             $sistem[] = [
